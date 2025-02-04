7237552e451117126aae7daf7aac2883228c17fa
Sleepingoff <justjh30@gmail.com>
2025-02-05 08:58:13 +0900

    FriendshipUIStateHandler 클래스 리팩토링 및 데이터 처리 메서드 개선

--- NPCSchedulers/Store/FriendshipUIStateHandler.cs
+++ NPCSchedulers/Store/FriendshipUIStateHandler.cs
@@ -4,64 +4,71 @@ using StardewValley;
 
 namespace NPCSchedulers.Store
 {
-    public class FriendshipUIStateHandler : IUIStateHandler<(string npcName, int heartLevel)>
+    public class FriendshipUIStateHandler : BaseUIStateHandler<Dictionary<string, int>>
     {
         private Dictionary<string, int> friendshipConditions = new();
+        private List<string> villagers = new();
 
-        public void LoadData()
+        public FriendshipUIStateHandler(string npcName, string scheduleKey) : base(npcName, scheduleKey)
         {
-            if (UIStateManager.Instance.CurrentNPC != null && UIStateManager.Instance.EditedScheduleKey != null)
-            {
-                FriendshipConditionEntry condition = ScheduleDataManager.GetFriendshipCondition(
-                    UIStateManager.Instance.CurrentNPC.Name,
-                    UIStateManager.Instance.EditedScheduleKey);
-
-                friendshipConditions = condition?.Condition ?? new Dictionary<string, int>();
-            }
-            else
-            {
-                friendshipConditions.Clear();
-            }
+            InitData();
         }
 
-        public void SaveData((string npcName, int heartLevel) data)
+        public override void InitData()
         {
-            if (UIStateManager.Instance.CurrentNPC != null)
+            FriendshipConditionEntry condition = ScheduleDataManager.GetFriendshipCondition(npcName, scheduleKey);
+            friendshipConditions = condition?.Condition ?? new Dictionary<string, int>();
+            villagers = Utility.getAllCharacters().Where(npc => npc.IsVillager).Select(npc => npc.Name).ToList();
+
+            foreach (var villager in villagers)
             {
-                friendshipConditions[data.npcName] = data.heartLevel;
-                UpdateScheduleCondition();
+                if (!friendshipConditions.ContainsKey(villager))
+                {
+                    friendshipConditions.Add(villager, 0);
+                }
             }
         }
-
-        public void UpdateData((string npcName, int heartLevel) data)
+        public override Dictionary<string, int> GetData()
         {
-            if (friendshipConditions.ContainsKey(data.npcName))
-            {
-                friendshipConditions[data.npcName] = data.heartLevel;
-                UpdateScheduleCondition();
-            }
+            return friendshipConditions;
         }
 
-        public void DeleteData((string npcName, int heartLevel) data)
+        public override void SaveData(Dictionary<string, int> data)
         {
-            if (friendshipConditions.ContainsKey(data.npcName))
-            {
-                friendshipConditions.Remove(data.npcName);
-                UpdateScheduleCondition();
-            }
+            friendshipConditions = data;
         }
 
-        private void UpdateScheduleCondition()
+        public override void UpdateData(Dictionary<string, int> data)
         {
-            if (UIStateManager.Instance.CurrentNPC != null && UIStateManager.Instance.EditedScheduleKey != null)
+            var conditions = friendshipConditions;
+            foreach (var key in conditions.Keys)
             {
-                var newFriendshipEntry = new FriendshipConditionEntry(
-                    UIStateManager.Instance.CurrentNPC.Name,
-                    UIStateManager.Instance.EditedScheduleKey,
-                    friendshipConditions);
-
-                ScheduleDataManager.UpdateFriendshipCondition(UIStateManager.Instance.CurrentNPC.Name, UIStateManager.Instance.EditedScheduleKey, newFriendshipEntry);
+                if (data.ContainsKey(key))
+                {
+                    int newLevel = data[key];
+                    if (newLevel >= 0 && newLevel <= 14)
+                    {
+                        conditions[key] = newLevel;
+                    }
+                }
             }
+            SaveData(conditions);
+        }
+
+        //지우고 싶은 npc이름을 data의 키로 등록하여 호출
+        public override void DeleteData(Dictionary<string, int> data)
+        {
+            foreach (var npc in data.Keys)
+            {
+                friendshipConditions.Remove(npc);
+            }
+        }
+        public Dictionary<string, int> FilterData(Dictionary<string, int> friendshipCondition)
+        {
+            Dictionary<string, int> newFriendshipConditionEntry = friendshipCondition;
+            var target = newFriendshipConditionEntry.Where(value => value.Value != 0);
+            newFriendshipConditionEntry = target.ToDictionary(pair => pair.Key, pair => pair.Value);
+            return newFriendshipConditionEntry;
         }
     }
 }
